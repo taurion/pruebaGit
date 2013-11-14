92139b5a0789c3b6c2fe29b0e71130e8a4853943
Jose <>
2013-11-14 16:34:21 +0100

    Commit rama version 1.0 prueba

--- src/pruebagit/PruebaGit.java
+++ src/pruebagit/PruebaGit.java
@@ -15,7 +15,7 @@ public class PruebaGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola");
-        System.out.println("Adios");
+        System.out.println("Hola caracola");
+        System.out.println("Adios caracol");
     }
 }
