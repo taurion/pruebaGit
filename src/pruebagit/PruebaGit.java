584634036c77b8984f55c497340d4f7a90e1e8d2
Jose <>
2013-11-14 16:17:23 +0100

    Segundo Commit

--- src/pruebagit/PruebaGit.java
+++ src/pruebagit/PruebaGit.java
@@ -16,5 +16,6 @@ public class PruebaGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola");
+        System.out.println("Adios");
     }
 }
